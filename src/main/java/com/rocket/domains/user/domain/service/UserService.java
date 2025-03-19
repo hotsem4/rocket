d15b8f3c8353d5671ab0fa3a87df0b293b21f7a8
Bumsu Choe <hotsem100@gmail.com>
2025-03-20 00:17:33 +0900

    feat(service): UserService에 인증 메서드 추가 및 회원가입 반환 타입 변경
    UserService 인터페이스에 사용자 인증(authenticate) 메서드를 추가하고,
    회원가입 메서드(saveUser)의 반환 타입을 User로 변경하였습니다.
    
    - 로그인 시 비밀번호 인증이 필요해짐에 따라 authenticate 메서드를 추가하였습니다.
    - 기존 saveUser의 반환 타입이 Boolean에서 User 엔티티로 변경되어 생성된 사용자 정보를 반환하도록 하였습니다.
    
    고민한 점:
    - 비밀번호 암호화 로직이 서비스에서 처리되면서, 회원가입 성공 여부만 Boolean으로 반환하는 것보다 User 엔티티 전체를 반환하는 것이 이후 확장성 및 유연성 측면에서 더 좋다고 판단하여 변경했습니다.
    - authenticate 메서드의 추가로 인증 로직이 서비스 계층에서 관리될 수 있도록 했습니다.

--- src/main/java/com/rocket/domains/user/domain/service/UserService.java
+++ src/main/java/com/rocket/domains/user/domain/service/UserService.java
@@ -4,12 +4,13 @@ import com.rocket.domains.user.application.dto.response.UserDTO;
 import com.rocket.domains.user.application.dto.request.UserRegisterDTO;
 import com.rocket.domains.user.application.dto.request.UserUpdateDTO;
 
+import com.rocket.domains.user.domain.entity.User;
 import java.util.List;
 import org.springframework.stereotype.Service;
 
 @Service
 public interface UserService {
-  Boolean saveUser(UserRegisterDTO dto);
+  User saveUser(UserRegisterDTO dto);
 
   UserDTO findByEmail(String email);
 
@@ -18,4 +19,6 @@ public interface UserService {
   UserDTO updateByEmail(UserUpdateDTO dto);
 
   boolean deleteByEmail(String email);
+
+  Boolean authenticate(String email, String rawPassword);
 }
