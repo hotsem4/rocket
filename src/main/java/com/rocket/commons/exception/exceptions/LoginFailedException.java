ee37998915b1b7f270daed4a208760ad72943327
Bumsu Choe <hotsem100@gmail.com>
2025-03-23 11:04:54 +0900

    feat(exception): 로그인 실패 시 사용할 LoginFailedException 추가
    - 인증 과정에서 이메일 또는 비밀번호가 일치하지 않을 경우를 명확하게 구분하기 위해 LoginFailedException 추가
    - UserService에서 로그인 실패 시 이 예외를 통해 클라이언트에 일관된 메시지 제공 가능

---
+++ src/main/java/com/rocket/commons/exception/exceptions/LoginFailedException.java
@@ -0,0 +1,9 @@
+package com.rocket.commons.exception.exceptions;
+
+public class LoginFailedException extends RuntimeException {
+
+  public LoginFailedException(String message) {
+    super(message);
+  }
+
+}
