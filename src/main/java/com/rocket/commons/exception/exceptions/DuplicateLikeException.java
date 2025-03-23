10a754d9930819e96c65d93d3134a3d6e4f20b5f
Bumsu Choe <hotsem100@gmail.com>
2025-03-23 21:54:32 +0900

    feat(common): DuplicateLikeException 예외 클래스 추가
    - 사용자가 동일 게시글에 중복으로 좋아요를 누를 경우를 처리하기 위한
      RuntimeException 기반의 커스텀 예외 클래스 정의
    - PostLikeService 내 중복 여부 확인 로직에서 사용 예정
    
    고민한 점:
    - 기존의 IllegalStateException 또는 IllegalArgumentException을 재사용할지,
      아니면 도메인별로 커스텀 예외를 분리할지 고민함
    - 중복 좋아요는 명확한 도메인 이벤트이므로, 향후 전역 예외 처리기에서 구체적인
      메시지를 전달하기 위해 별도 예외 클래스로 분리하는 방향을 선택

---
+++ src/main/java/com/rocket/commons/exception/exceptions/DuplicateLikeException.java
@@ -0,0 +1,9 @@
+package com.rocket.commons.exception.exceptions;
+
+public class DuplicateLikeException extends RuntimeException {
+
+  public DuplicateLikeException(String message) {
+    super(message);
+  }
+
+}
