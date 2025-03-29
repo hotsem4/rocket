2df479cbd16e017caceb712e06681aacd8fa8507
Bumsu Choe <hotsem100@gmail.com>
2025-03-29 22:28:03 +0900

    feat(exception): 유효하지 않은 토큰 예외 처리 클래스 추가
    JWT 인증 과정에서 발생할 수 있는 다양한 토큰 오류 상황을 처리하기 위해
    `InvalidTokenException` 클래스를 추가했습니다.
    
    - `RuntimeException`을 상속하여 런타임 시 발생할 수 있는 예외로 정의
    - 오류 메시지를 인자로 받아 상세한 원인 전달 가능
    
    도입 배경:
    - AccessToken 또는 RefreshToken이 유효하지 않을 경우 일관된 방식으로 예외 처리 필요
    - 예외 메시지를 통해 클라이언트와 개발자 모두에게 명확한 디버깅 정보 제공
    
    고민했던 점:
    - 어떤 상황을 '유효하지 않음'으로 판단할 것인지 기준을 명확히 할 필요가 있었음
      → 형식 오류, 만료, Redis 미일치 등 모두 이 예외로 통합하여 관리하기로 결정

---
+++ src/main/java/com/rocket/commons/exception/exceptions/InvalidTokenException.java
@@ -0,0 +1,9 @@
+package com.rocket.commons.exception.exceptions;
+
+public class InvalidTokenException extends RuntimeException {
+
+  public InvalidTokenException(String message) {
+    super(message);
+  }
+
+}
