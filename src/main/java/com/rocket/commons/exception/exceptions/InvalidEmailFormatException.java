072e51b3d83e7cdbddc70de509120207c8d9992a
Bumsu Choe <hotsem100@gmail.com>
2025-03-30 15:38:50 +0900

    feat(exception): 잘못된 이메일 형식 예외 클래스 추가
    InvalidEmailFormatException 클래스를 추가하여 이메일 형식이 유효하지 않을 경우 사용할 수 있도록 했습니다.
    - RuntimeException을 상속받아 커스텀 예외 구현
    - 생성자에서 사용자 정의 메시지를 전달받아 예외 메시지 지정 가능
    - 향후 회원가입, 로그인 등에서 이메일 유효성 검사 로직과 함께 활용 예정
    
    이메일 형식 검증 실패를 명확하게 구분된 예외로 처리하기 위해 커스텀 예외 클래스를 도입했습니다.
    
    고민한 점:
    - 예외 이름을 `InvalidEmailFormatException`으로 명확하게 지정하여, 용도와 의미를 직관적으로 표현했습니다.
    - 이메일 검증 책임을 어디에 둘지(Validator vs. Controller vs. Service) 구조적으로 정리할 필요가 있다고 판단했습니다.

---
+++ src/main/java/com/rocket/commons/exception/exceptions/InvalidEmailFormatException.java
@@ -0,0 +1,9 @@
+package com.rocket.commons.exception.exceptions;
+
+public class InvalidEmailFormatException extends RuntimeException {
+
+  public InvalidEmailFormatException(String message) {
+    super(message);
+  }
+
+}
