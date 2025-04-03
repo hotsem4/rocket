c536208d25a49c5f1140fea0c40d48a3b227a6a3
Bumsu Choe <hotsem100@gmail.com>
2025-04-03 13:24:56 +0900

    feat(user): User 엔티티에 Role 필드 추가

--- src/main/java/com/rocket/domains/user/domain/entity/User.java
+++ src/main/java/com/rocket/domains/user/domain/entity/User.java
@@ -1,7 +1,9 @@
 package com.rocket.domains.user.domain.entity;
 
 import com.rocket.domains.user.application.dto.request.AddressRequest;
+import com.rocket.domains.user.application.dto.request.UserUpdateRequest;
 import com.rocket.domains.user.domain.enums.Gender;
+import com.rocket.domains.user.domain.enums.Role;
 import io.micrometer.core.instrument.step.StepRegistryConfig;
 import jakarta.persistence.Column;
 import jakarta.persistence.Embedded;
@@ -62,7 +64,7 @@ public class User {
 
   @NotNull(message = "성별은 필수 입력값입니다.")
   @Column(name = "gender", nullable = false)
-  @Comment("나이")
+  @Comment("성별")
   @Enumerated(EnumType.STRING)
   private Gender gender;
 
@@ -73,10 +75,16 @@ public class User {
 
   @NotNull(message = "전화번호는 필수 입력값입니다.")
   @NotBlank
+  @Column(name ="phoneNumber", nullable = false)
   private String phoneNumber;
 
+
+  @Enumerated(EnumType.STRING)
+  @Comment("역할")
+  private Role role;
+
   private User(String email, String password, int age, Gender gender, Address address,
-      String nickname, String phoneNumber) {
+      String nickname, String phoneNumber, Role role) {
     this.email = email;
     this.password = password;
     this.age = age;
@@ -84,14 +92,15 @@ public class User {
     this.address = address;
     this.nickname = nickname;
     this.phoneNumber = phoneNumber;
+    this.role = role;
   }
 
   // @VisibleForTesting
   public static User createWithIdForTest(
       Long id, String email, String password, int age, Gender gender, Address address,
-      String nickname, String phoneNumber
+      String nickname, String phoneNumber, Role role
   ) {
-    User user = new User(email, password, age, gender, address, nickname, phoneNumber);
+    User user = new User(email, password, age, gender, address, nickname, phoneNumber, role);
     user.id = id;
     return user;
   }
@@ -105,9 +114,10 @@ public class User {
       @NotNull Gender gender,
       @Valid @NotNull Address address,
       @NotBlank String nickname,
-      @NotBlank String phoneNumber
+      @NotBlank String phoneNumber,
+      @NotBlank Role role
   ) {
-    return new User(email, password, age, gender, address, nickname, phoneNumber);
+    return new User(email, password, age, gender, address, nickname, phoneNumber, role);
   }
 
 
